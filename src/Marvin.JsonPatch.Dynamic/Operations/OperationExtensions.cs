5f94ff0fd1680b1f1833589c0ff804881751ee99
KevinDockx <kevin.dockx@gmail.com>
2015-05-29 17:23:35 +0200

    basic classes and framework

---
+++ src/Marvin.JsonPatch.Dynamic/Operations/OperationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Marvin.JsonPatch.Adapters;
+using Marvin.JsonPatch.Dynamic.Adapters;
+using Marvin.JsonPatch.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marvin.JsonPatch.Dynamic.Operations
+{
+    public static class OperationExtensions
+    {
+        public static void Apply(this Operation operation, dynamic dynamic, IDynamicObjectAdapter adapter)
+        {
+            // todo
+        }
+    }
+}
