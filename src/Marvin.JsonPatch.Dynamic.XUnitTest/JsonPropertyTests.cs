8bfc4662a9b2ee968c7690f0c26ec6e549244a7b
KevinDockx <kevin.dockx@gmail.com>
2016-09-05 16:01:38 +0200

    Update references, properties

---
+++ src/Marvin.JsonPatch.Dynamic.XUnitTest/JsonPropertyTests.cs
@@ -0,0 +1,129 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Marvin.JsonPatch.Dynamic.XUnitTest
+{
+    public class JsonPropertyTests
+    {
+        [Fact]
+        public void HonourJsonPropertyOnSerialization()
+        {
+            // create patch
+            JsonPatchDocument<JsonPropertyDTO> patchDoc = new JsonPatchDocument<JsonPropertyDTO>();
+            patchDoc.Add(p => p.Name, "Kevin");
+
+            var serialized = JsonConvert.SerializeObject(patchDoc);
+            // serialized value should have "AnotherName" as path
+            // deserialize to a JsonPatchDocument<JsonPropertyWithAnotherNameDTO> to check
+            var deserialized =
+                JsonConvert.DeserializeObject<JsonPatchDocument<JsonPropertyWithAnotherNameDTO>>(serialized);
+
+            Assert.Equal(deserialized.Operations.First().path, "/anothername");
+        }
+
+        [Fact]
+        public void CanApplyToDifferentlyTypedClassWithPropertyMatchingJsonPropertyName()
+        {
+            // create patch
+            JsonPatchDocument<JsonPropertyDTO> patchDocToSerialize =
+                new JsonPatchDocument<JsonPropertyDTO>();
+            patchDocToSerialize.Add(p => p.Name, "Kevin");
+
+            // the patchdoc will deserialize to "anothername".  We should thus be able to apply 
+            // it to a class that HAS that other property name.
+
+            var doc = new JsonPropertyWithAnotherNameDTO()
+            {
+                AnotherName = "InitialValue"
+            };
+
+            var serialized = JsonConvert.SerializeObject(patchDocToSerialize);
+            var deserialized =
+                JsonConvert.DeserializeObject<JsonPatchDocument<JsonPropertyWithAnotherNameDTO>>
+                (serialized);
+
+            deserialized.ApplyTo(doc);
+
+            Assert.Equal(doc.AnotherName, "Kevin");
+        }
+
+        [Fact]
+        public void CanApplyToSameTypedClassWithMatchingJsonPropertyName()
+        {
+            // create patch
+            JsonPatchDocument<JsonPropertyDTO> patchDocToSerialize =
+                new JsonPatchDocument<JsonPropertyDTO>();
+            patchDocToSerialize.Add(p => p.Name, "Kevin");
+
+            // the patchdoc will deserialize to "anothername".  As JsonPropertyDTO has
+            // a JsonProperty signifying that "Name" should be deseriallized from "AnotherName",
+            // we should be able to apply the patchDoc.
+
+            var doc = new JsonPropertyDTO()
+            {
+                Name = "InitialValue"
+            };
+
+            var serialized = JsonConvert.SerializeObject(patchDocToSerialize);
+            var deserialized =
+                JsonConvert.DeserializeObject<JsonPatchDocument<JsonPropertyDTO>>
+                (serialized);
+
+            deserialized.ApplyTo(doc);
+
+            Assert.Equal(doc.Name, "Kevin");
+        }
+
+        [Fact]
+        public void HonourJsonPropertyOnApplyForAdd()
+        {
+            var doc = new JsonPropertyDTO()
+            {
+                Name = "InitialValue"
+            };
+
+            // create patch
+            //var patchDoc = new JsonPatchDocument<JsonPropertyDTO>();
+            //patchDoc.Add(p => p.Name, "Kevin");
+
+            // serialization should serialize to "AnotherName"
+            var serialized = "[{\"value\":\"Kevin\",\"path\":\"/AnotherName\",\"op\":\"add\"}]";
+            // that means we can deserialize to JsonPatchDocument<JsonPropertyWithAnotherNameDTO>      
+            var deserialized =
+                JsonConvert.DeserializeObject<JsonPatchDocument<JsonPropertyDTO>>(serialized);
+
+            deserialized.ApplyTo(doc);
+
+            Assert.Equal("Kevin", doc.Name);
+        }
+
+
+        [Fact]
+        public void HonourJsonPropertyOnApplyForRemove()
+        {
+            var doc = new JsonPropertyDTO()
+            {
+                Name = "InitialValue"
+            };
+
+            // create patch
+            //var patchDoc = new JsonPatchDocument<JsonPropertyDTO>();
+            //patchDoc.Add(p => p.Name, "Kevin");
+
+            // serialization should serialize to "AnotherName"
+            var serialized = "[{\"path\":\"/AnotherName\",\"op\":\"remove\"}]";
+            // that means we can deserialize to JsonPatchDocument<JsonPropertyWithAnotherNameDTO>      
+            var deserialized =
+                JsonConvert.DeserializeObject<JsonPatchDocument<JsonPropertyDTO>>(serialized);
+
+            deserialized.ApplyTo(doc);
+
+            Assert.Equal(null, doc.Name);
+        }
+    }
+}
--- src/Marvin.JsonPatch.Dynamic/Properties/AssemblyInfo.cs
+++ src/Marvin.JsonPatch.Dynamic/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kevin Dockx (Marvin)")]
 [assembly: AssemblyProduct("Dynamically typep object support for Marvin.JsonPatch")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,9 +26,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0")]
-[assembly: AssemblyFileVersion("0.9.0")]
-[assembly: AssemblyInformationalVersion("0.9.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
  
 
 // Setting ComVisible to false makes the types in this assembly not visible 
